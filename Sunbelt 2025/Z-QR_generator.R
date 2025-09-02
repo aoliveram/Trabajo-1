d01422a82f94c7a9fbbcb649cb0172770fb8e62e
aoliveram <anibal.olivera.m@gmail.com>
2025-09-02 14:49:09 -0400

    updating

--- Sunbelt 2025/Z-QR_generator.R
+++
@@ -1,5 +0,0 @@
-#install.packages("qrcode")
-
-library(qrcode)
-qr_code("https://github.com/aoliveram/Trabajo-1/tree/main/Sunbelt%202025") |>
-  generate_svg("diffusion_of_innovations_heatmaps.svg", background = "transparent")
